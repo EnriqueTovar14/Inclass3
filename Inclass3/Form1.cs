205c2889f788ba1eb325b2db266a1293c544e76b
enrique tovar <enriquektovar14@gmail.com>
2019-04-23 14:21:17 -0700

    commit

--- Inclass3/Form1.cs
+++ Inclass3/Form1.cs
@@ -16,5 +16,26 @@ namespace Inclass3
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                double weightE, weightM;
+                weightE = double.Parse(textBox1.Text);
+                weightM = weightE / 9.81 * 3.711;
+                textBox2.Text = weightM.ToString();
+                textBox2.Text = String.Format("{0:0.000}", weightM);
+            }
+
+            catch { 
+            MessageBox.Show("Please try again using a number");
+
+        }
+        }
+        
     }
-}
+}    
+
+
+
